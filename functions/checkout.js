dc32a40d977ed5cb99839b6224ed561e2930be55
Zach Royer <zroyer@utexas.edu>
2020-04-22 12:52:12 -0500

    accepting payments

---
+++ functions/checkout.js
@@ -0,0 +1,37 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+    const { payment } = JSON.parse(event.body);
+
+    const paymentAmount = payment * 100;
+    const feesAmount = paymentAmount * 0.03;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      success_url: `${process.env.URL}/`,
+      cancel_url: `${process.env.URL}/`,
+      line_items: [
+        {
+            name: 'Payment',
+            amount: paymentAmount,
+            currency: 'usd',
+            quantity: 1,
+        },
+        {
+            name: 'Fees',
+            amount: feesAmount,
+            currency: 'usd',
+            quantity: 1,
+        },
+      ],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        sessionId: session.id,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      }),
+    };
+  };
