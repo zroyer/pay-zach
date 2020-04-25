a13f1bab37adf3f2651314a69581accf1dd4e649
Zach Royer <zroyer@utexas.edu>
2020-04-25 14:11:42 -0500

    adds routing

---
+++ src/pages/error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { url } from "@sveltech/routify";
+</script>
+
+<main>
+  <header>
+    <h2>Uh oh, something went wrong...</h2>
+    <a href={$url('/index')}>Try again?</a>
+  </header>
+</main>
---
+++ src/pages/success.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { url } from "@sveltech/routify";
+</script>
+
+<main>
+    <h2>Thanks!</h2>
+    <a href={$url('/index')}>Feeling generous?</a>
+</main>
